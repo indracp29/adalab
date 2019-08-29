49af1c4c99c2aa148845641ad334cc215a271333
indracp29 <53854527+indracp29@users.noreply.github.com>
2019-08-29 15:57:05 +0530

    Add files via upload

---
+++ quicksort.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+void swap(int* a, int* b)
+{
+	int t = *a;
+	*a = *b;
+	*b = t;
+}
+int partition (int arr[], int low, int high)
+{
+	int pivot = arr[high];
+	int i = (low - 1);
+	for (int j = low; j <= high- 1; j++)
+	{	
+	if (arr[j] <= pivot)
+	{
+		i++;
+		swap(&arr[i], &arr[j]);
+        }
+      }
+
+
+  swap(&arr[i + 1], &arr[high]);
+  return (i + 1);
+}
+
+
+
+void quickSort(int arr[], int low, int high)
+ {
+   if (low < high)
+    {
+      int pi = partition(arr, low, high);
+      quickSort(arr, low, pi - 1);
+      quickSort(arr, pi + 1, high);
+    }
+}
+
+ 
+
+int main()
+{
+  int arr[10],i,n;
+	printf("Enter the size of N:\n");
+	scanf("%d",&n);
+	printf("Enter the elements of array\n");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+  quickSort(arr, 0, n-1);
+  printf("The sorted array is: \n");
+ 
+   for (i=0; i < size; i++)
+   printf("%d ", arr[i]);
+   printf("\n");
+
+  return 0;
+}
